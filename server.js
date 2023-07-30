f827526873d452fac5a1fa61f045d63a658b5788
Chandrabhan Rahangdale <nikhilrahangdale786@gmail.com>
2023-07-31 00:46:56 +0530

    app getting ready

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
+const multer = require('multer')
+const upload =multer({dest:'uploads/'})
+const {mergerpdfs} = require('./merge');
+
+app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! my name is nick and it is auto save')
+  res.sendFile(path.join(__dirname , 'template/index.html'))
+})
+
+app.post('/merge' , upload.array('pdfs', 2 ) , async(req ,res ,next)=>{
+    console.log(req.files)
+    let d = await mergerpdfs(path.join(__dirname , req.files[0].path), path.join(__dirname , req.files[1].path))
+    res.redirect(`http://localhost:3000/static/${d}.pdf` )
+    // res.send({data:req.files})
 })
 
 app.listen(port, () => {
